55e0392f2bbe4801f71347b46f71208c1d9730fb
Daniel <danieldanila26@gmail.com>
2019-08-25 21:35:19 +0300

    comm

--- src/main/java/ro/itschool/Test.java
+++ src/main/java/ro/itschool/Test.java
@@ -1,35 +1,18 @@
 package ro.itschool;
 
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(new int[]{getDiagonalSum(new int[][]{{3, 62, 12, 0}, {3, 10, 3, 47}, {91, 2, 87, 4}, {23, 1, 0, 55}})}));
+
+        System.out.println();
 
     }
-        public static int getDiagonalSum(int[][] matrix){
-        int a=matrix.length;
-        int b=matrix[0].length;
-        int sum=0;
-
-        for (int i=0;i<a;i++){
-            for (int j=0;j<b;j++){
-                if (i==j){
-                    sum=sum+matrix[i][j];
-                }
-            }
-        }
-        return sum;
+    public static int[][] getMatrixSum(int[][] matrix1, int[][] matrix2){
 
     }
-
-
-
-
 
 }
--- src/main/java/ro/itschool/curs22/StudentMain.java
+++ src/main/java/ro/itschool/curs22/StudentMain.java
@@ -3,5 +3,7 @@ package ro.itschool.curs22;
 import java.util.List;
 
 public class StudentMain {
+    public static void main(String[] args) {
 
+    }
 }
